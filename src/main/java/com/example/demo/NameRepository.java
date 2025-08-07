d5b1134871b0f3f70e4864ceba487fdc9a132d70
sumanpalakolu <sumanpalakolu@gmail.com>
2025-08-06 23:57:41 -0400

    Delete src/main/java/com/example/demo/NameRepository.java

--- src/main/java/com/example/demo/NameRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-// JpaRepository provides CRUD operations for NameEntity
-@Repository
-public interface NameRepository extends JpaRepository<NameEntity, Long> {
-}
